e9111ebca0e54030a22b2f0b3a7be426f6504a3f
MichaelCaraccio <michael.caraccio@gmail.com>
2015-04-30 16:48:09 +0200

    Get pos and get folder from tagged folder

--- File.py
+++ File.py
@@ -13,9 +13,27 @@ class File:
         self.filesContentUnique = []
         self.filesMerged = []
         self.folders = ["pos", "neg"]
+        self.taggedFolders = ["tagged/pos", "tagged/neg"]
         self.fileContentList = {}
 
+    # -----------------------------------------------------------
+    # Get words from tagged folder
+    # -----------------------------------------------------------
+    def from_tagged_folder(self):
+        for folder in self.taggedFolders:
+            for file in os.listdir(folder):
+                if file.endswith(".txt"):
+                    self.fileContentList[file] = self.wordFromFileWithDelimiter(folder+"/"+file,'\t')
+                    # Get all words from file and Store them in an array
+                    self.filesContent.append(self.fileContentList[file])
+        self.mergeList()
+        self.unique()
+        self.clean()
+        return self.filesMerged
 
+    # -----------------------------------------------------------
+    # Get words from pos and get folder at root folder
+    # -----------------------------------------------------------
     def from_folder(self):
         for folder in self.folders:
             for file in os.listdir(folder):
@@ -35,11 +53,9 @@ class File:
         for sentences in self.filesContent:
             for words in sentences:
                 self.filesMerged.append(words)
-        #print(len(self.filesMerged))
 
     def unique(self):
         self.filesMerged = list(set(self.filesMerged))
-        #print(len(self.filesMerged))
 
     def clean(self):
         for words in self.filesMerged:
@@ -48,7 +64,6 @@ class File:
         print(len(self.filesMerged))
 
     def wordFromFile(self, filename):
-        #print(filename)
         return re.findall(r'\w+', open(filename,'r',encoding='utf-8').read())
 
     def initFrenchST(self):
@@ -57,5 +72,11 @@ class File:
     def wordFromFileWithDelimiter(self, filename, delimite):
         with open(filename, 'r') as f:
             reader = csv.reader(f, delimiter=delimite)
+            listword = []
             for row in reader:
-                print(row)
+                try:
+                    listword.append(row[2])
+                except:
+                    print("delimiter faux")
+
+            return listword

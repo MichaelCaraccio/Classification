08c1f8547346839af91727c666788fa9841975ee
MichaelCaraccio <michael.caraccio@gmail.com>
2015-05-01 19:45:20 +0200

    Bayes a jour

--- bayes.py
+++ bayes.py
@@ -1,1 +1,20 @@
-__author__ = 'michaelcaraccio'
+__author__ = 'Christophe Bolinhas et Michael Caraccio'
+
+from File import File
+
+def getWords():
+    fileManager = File()
+    return fileManager.from_folder()
+
+def handleData():
+    listWords = getWords()
+
+
+
+if __name__ == "__main__":
+
+    print("\n-----------------------------------------------------")
+    print("Classification de mots utilisant un réseau de neurone")
+    print("-----------------------------------------------------\n")
+
+
